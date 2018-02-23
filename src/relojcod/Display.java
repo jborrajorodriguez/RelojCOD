c57a84ae962b24e270b06007715f169161438ade
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-23 09:50:47 +0100

    Mostrar hora

--- src/relojcod/Display.java
+++ src/relojcod/Display.java
@@ -1,7 +1,22 @@
 package relojcod;
+
+import javax.swing.JOptionPane;
+
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class Display {
+
+    /**
+     * Metodo estatico que se encarga de mostrar la hora
+     *
+     * @param a horas
+     * @param b minutos
+     * @param c segundos
+     */
+    public static void mostrar(int a,int b,int c) {
+//        JOptionPane.showMessageDialog(null,a+":"+b+":"+c);
+        System.out.println(a+":"+b+":"+c);
+    }
 
 }
