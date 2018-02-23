8ed4d929459332094f84ce54bf010dbbf012e9b4
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-23 09:24:23 +0100

    Metodo no se usa

--- src/relojcod/RelojFun.java
+++ src/relojcod/RelojFun.java
@@ -1,7 +1,24 @@
 package relojcod;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class RelojFun {
+//
+//    Calendar calendario=new GregorianCalendar();
+//    private int hora, minutos, segundos;
+//
+//    public void hora() {
+//
+//        hora=calendario.get(Calendar.HOUR_OF_DAY);
+//        minutos=calendario.get(Calendar.MINUTE);
+//        segundos=calendario.get(Calendar.SECOND);
+//        Display.mostrar(hora,minutos,segundos);
+//      
+//
+//    }
 
 }
