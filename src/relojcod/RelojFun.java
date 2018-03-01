1d8abe5dadd626658801cd4f681bfde70312ca30
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-03-01 08:48:09 +0100

    Funcion de reloj acabada

--- src/relojcod/RelojFun.java
+++ src/relojcod/RelojFun.java
@@ -1,54 +1,37 @@
 package relojcod;
 
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import javax.swing.JOptionPane;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
-public class RelojFun implements Runnable {
+public class RelojFun{
 
-    String hora, minutos, segundos, ampm;
-    Calendar calendario;
-    Thread h1;
+  private static int h, m, s;
+    private static Date d;
 
-    public RelojFun() {
-//        initComponents();
-        h1 = new Thread(this);
-        h1.start();
-    }
-
-    @Override
-    public void run() {
-        Thread ct = Thread.currentThread();
-        while (ct == h1) {
-            calcula();
-            System.out.println((hora + ":" + minutos + ":" + segundos + " " + ampm));
-            //JOptionPane.showMessageDialog(null,hora + ":" + minutos + ":" + segundos + " " + ampm);
+    public static void tiempo(){
+        do{
+            
             try {
+                d=new Date();
+                h=d.getHours();
+                m=d.getMinutes();
+                s=d.getSeconds();
+                Display.mostrar(h, m, s);
                 Thread.sleep(1000);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException ex) {
+                Logger.getLogger(RelojFun.class.getName()).log(Level.SEVERE,null,ex);
             }
-        }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    public void calcula() {
-        Calendar calendario = new GregorianCalendar();
-        Date fechaHoraActual = new Date();
+            
+        }while(1==1);
 
-
-        calendario.setTime(fechaHoraActual);
-        ampm = calendario.get(Calendar.AM_PM) == Calendar.AM ? "AM" : "PM";
-
-        if (ampm.equals("PM")) {
-            int h = calendario.get(Calendar.HOUR_OF_DAY) - 12;
-            hora = h > 9 ? "" + h : "0" + h;
-        } else {
-            hora = calendario.get(Calendar.HOUR_OF_DAY) > 9 ? "" + calendario.get(Calendar.HOUR_OF_DAY) : "0" + calendario.get(Calendar.HOUR_OF_DAY);
-        }
-        minutos = calendario.get(Calendar.MINUTE) > 9 ? "" + calendario.get(Calendar.MINUTE) : "0" + calendario.get(Calendar.MINUTE);
-        segundos = calendario.get(Calendar.SECOND) > 9 ? "" + calendario.get(Calendar.SECOND) : "0" + calendario.get(Calendar.SECOND);
     }
 }
+
+
+
