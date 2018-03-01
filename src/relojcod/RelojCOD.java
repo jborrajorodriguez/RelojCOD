233198608db7770cbb78c7d105ddb1458fdb44d7
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-03-01 09:39:07 +0100

    selector version 1

--- src/relojcod/RelojCOD.java
+++ src/relojcod/RelojCOD.java
@@ -1,12 +1,33 @@
 package relojcod;
+
+import javax.swing.JOptionPane;
+
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class RelojCOD {
 
     public static void main(String[] args) {
-        RelojFun.tiempo();
-   
+        int m=0;
+        do {   m=Integer.parseInt(JOptionPane.showInputDialog("Opcion"));
+            switch (m) {
+                case 1:
+                    RelojFun.tiempo();
+                    break;
+                case 2:
+                    RelojFun.Alarma();
+                    break;    
+                    
+                    
+                default:
+                    throw new AssertionError();
+            }
+            
+        }
+        while (m!=0);
+      
+      
+        
         
     }
 
