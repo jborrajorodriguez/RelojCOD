7ab0cf8e2fc98edb7a8cb4cb890b6931f0439272
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-03-09 08:43:02 +0100

    Nuevo metodo de hora

--- src/relojcod/RelojFun.java
+++ src/relojcod/RelojFun.java
@@ -1,11 +1,39 @@
 package relojcod;
 
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class RelojFun {
+    
+ private static int h, m, s;
+    private static Date d;
 
+    /**
+     * Metodo tiempo
+     * 
+     * Este metodo se encarga de contar el tiempo
+     */
+    public static void tiempo(){
+        
+        do{
+            try{
+                d=new Date();
+                h=d.getHours();
+                m=d.getMinutes();
+                s=d.getSeconds();
+                Display.mostrar(h, m, s);
+                Thread.sleep(1000);
+            }catch(InterruptedException ex){
+                Logger.getLogger(RelojFun.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }while(1==1);
+
+    }
    
     
 }
