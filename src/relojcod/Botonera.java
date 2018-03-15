f0e74adfdb51f22b964379c220211830b36be562
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-03-15 09:06:38 +0100

    Activar/desactivar alarma

--- src/relojcod/Botonera.java
+++ src/relojcod/Botonera.java
@@ -9,10 +9,11 @@ public class Botonera {
     public static void selector(){
         int m;
         do { 
-            m=Integer.parseInt(JOptionPane.showInputDialog("Selecciona lo que quieras"));
+            m=Integer.parseInt(JOptionPane.showInputDialog("Selecciona lo que quieras :\n1 Hora\n2 Alarma on-off\n3 Poner alarma"));
             
             switch (m) {
                 case 1:
+                    
                     RelojFun.tiempo();
                     
                     
