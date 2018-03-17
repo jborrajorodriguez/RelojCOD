0fd71350aa057edd74d9821e8227235d95367bf6
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-03-17 11:29:48 +0100

    Nuevos metodos

--- src/relojcod/Display.java
+++ src/relojcod/Display.java
@@ -18,5 +18,22 @@ public class Display {
 //        JOptionPane.showMessageDialog(null,a+":"+b+":"+c);
         System.out.println(a + ":" + b + ":" + c);
     }
-
+    /**
+     * Metodo que muestra el mensaje de alarma activada
+     */
+    public static void activada(){
+        System.out.println("Activada");
+    }
+    /**
+     * Metodo que muestra el mensaje de alarma desactivada
+     */
+    public static void desactivada(){
+        System.out.println("Desactivada");
+    }
+    /**
+     * Metodo que muestra el mensaje de alarma sonando
+     */
+    public static void sonido(){
+        System.out.println("Alarma sonandooooooooo");
+    }
 }
