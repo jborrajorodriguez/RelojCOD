a03bb8f2d48242119d6490411ddde7118683afe1
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-03-17 11:25:34 +0100

    Reordenacion de codigo

--- src/relojcod/Botonera.java
+++ src/relojcod/Botonera.java
@@ -6,33 +6,35 @@ import javax.swing.JOptionPane;
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class Botonera {
-    public static void selector(){
+
+    /**
+     * Metodo selector
+     *
+     * Este metodo es el encargado de mostrar un selector que actua como menú
+     * del reloj
+     */
+    public static void selector() {
         int m;
-        do { 
-            m=Integer.parseInt(JOptionPane.showInputDialog("Selecciona lo que quieras :\n1 Hora\n2 Alarma on-off\n3 Poner alarma"));
-            
+        do {
+            m = Integer.parseInt(JOptionPane.showInputDialog("Selecciona lo que quieras :\n1 Hora\n2 Alarma on-off\n3 Poner alarma"));
+
             switch (m) {
                 case 1:
-                    
+
                     RelojFun.tiempo();
-                    
-                    
+
                     break;
-                case 2:Alarma.encendido();
-                break;
-                case 3:Alarma.Alarma();
-                break;
+                case 2:
+                    Alarma.encendido();
+                    break;
+                case 3:
+                    Alarma.Alarma();
+                    break;
                 default:
                     throw new AssertionError();
             }
-            
-        }
-        while (true);
-    }
-    
-    public void añadirAlarma(){
-       
-        
+
+        } while (true);
     }
 
 }
